a65dab37f5af5ae5b6247a5db334a19373aa9be7
minte9 <catalinprescure@gmail.com>
2023-01-25 15:26:04 +0200

    mlearning-pages update

--- main/calculus/derivatives/derived_function.py
+++ main/calculus/derivatives/derived_function.py
@@ -1,6 +1,9 @@
 """ Derived function
     f(x) = ax^2
 
+Derived function f'(x) is pronounced "f prime of x"
+Instantaneous rate of change of y with respect to x at value x1
+
 Increment method
     y + Dy = a(x + Dx)^2
     y + Dy = ax^2 + 2axDx + a(Dx^2)
@@ -9,22 +12,24 @@ Increment method
 
 At limit Dx -> 0
     f'(x) = 2ax
-
-Derived function f'(x) is pronounced "f prime of x"
 """
 
 from sympy import *
 
 x = Symbol('x')
 t = Symbol('t')
+r = Symbol('r')
 
-a = 1
-f = a*x**2
-d = f.diff(x)
+a  = 1
+f  = a*x**2 # generic
+d1 = f.diff(x)
 
-print(f'f\'(x) = {d}')  # f'(x) = 2*x
+s  = 16*t**2 # speed
+d2 = s.diff(t)
 
-s = 16*t**2
-d = s.diff(t)
+A  = pi*r**2 # circle area
+d3 = A.diff(r)
 
-print(f's\'(t) = {d}')  # s'(x) = 32*x
+print(f'f\'(x) = {d1}')  # f'(x) = 2*x
+print(f's\'(t) = {d2}')  # s'(x) = 32*t
+print(f'A\'(r) = {d3}')  # A'(r) = 2*pi*r

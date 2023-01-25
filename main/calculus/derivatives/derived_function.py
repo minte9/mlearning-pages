d7c646d6b2f9a3d0d0bcac152c4e29990b9dff58
minte9 <catalinprescure@gmail.com>
2023-01-25 15:16:14 +0200

    mlearning-pages update

--- main/calculus/derivatives/derived_function.py
+++ main/calculus/derivatives/derived_function.py
@@ -21,12 +21,9 @@ a = 1
 f = a*x**2
 d = f.diff(x)
 
-print(f'f(x) = {f}')    # f(x) = x**2
 print(f'f\'(x) = {d}')  # f'(x) = 2*x
 
-a = 16
 s = 16*t**2
 d = s.diff(t)
 
-print(f's(t) = {s}')    # s(x) = 16*x**2
 print(f's\'(t) = {d}')  # s'(x) = 32*x

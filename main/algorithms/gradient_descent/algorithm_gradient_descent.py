9d47b9c7868a1c791ddd16e526dece64423d0d96
minte9 <catalinprescure@gmail.com>
2023-01-29 18:33:27 +0200

    mlearning-pages update

--- main/algorithms/gradient_descent/algorithm_gradient_descent.py
+++ main/algorithms/gradient_descent/algorithm_gradient_descent.py
@@ -26,11 +26,15 @@ def cost(theta, x, y):
 
 def gradient_descent(x, y, theta, lr, num_iterations):
     cost_history = np.zeros(num_iterations)
+
     for i in range(num_iterations):
+
         y_pred = np.dot(x, theta)
         error = y_pred - y
+
         theta = theta - (lr/len(y)) * np.dot(x.T, error)
         cost_history[i] = cost(theta, x, y)
+
     return theta, cost_history
 
 x = np.array([[1, 2], [1, 3], [1, 4], [1, 5]])
@@ -39,6 +43,7 @@ y = np.array([[7], [6], [5], [7]])
 theta = np.random.randn(2, 1)
 lr = 0.01
 num_iterations = 1000
+
 theta, cost_history = gradient_descent(x, y, theta, lr, num_iterations)
 
 print("Theta: ", theta) # [[4.55230192] [0.43431721]]

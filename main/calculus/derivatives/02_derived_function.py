19f4c0302ca061f21c689efa2236c3291f122b46
minte9 <catalinprescure@gmail.com>
2023-01-31 10:00:18 +0200

    mlearning-pages update

--- main/calculus/derivatives/02_derived_function.py
+++ main/calculus/derivatives/02_derived_function.py
@@ -1,8 +1,8 @@
-""" Derived function
-    f(x) = ax^2
+""" Derived function for f(x) = ax^2
+f'(x) is pronounced "f prime of x"
 
-Derived function f'(x) is pronounced "f prime of x"
-Instantaneous rate of change of y with respect to x at value x1
+It means instantaneous rate of change of y 
+with respect to x at value x1.
 
 Increment method
     y + Dy = a(x + Dx)^2
@@ -10,29 +10,28 @@ Increment method
     Dy = 2axDx + a(Dx^2)
     Dy/Dx = 2ax + aDx
 
-At limit Dx -> 0
+As Dx approaches the limit Dx -> 0, the derived is:
     f'(x) = 2ax
 """
 
 from sympy import *
 
-x = Symbol('x')
+# Derivative for falling speed s(t) = 16^2
 t = Symbol('t')
-r = Symbol('r')
-
-a  = 1
-f  = a*x**2 # generic
-d1 = f.diff(x)
-
-s  = 16*t**2 # speed
+s  = 16*t**2
 d2 = s.diff(t)
 
-A  = pi*r**2 # circle area
+# Derivative for circle area A(r) = pi*R^2
+r = Symbol('r')
+A  = pi*r**2
 d3 = A.diff(r)
+
+# Derivative for f(x) = ax^2
+x = Symbol('x')
+a  = 1
+f  = a*x**2
+d1 = f.diff(x)
 
 print(f'f\'(x) = {d1}')  # f'(x) = 2*x
 print(f's\'(t) = {d2}')  # s'(x) = 32*t
 print(f'A\'(r) = {d3}')  # A'(r) = 2*pi*r
-
-f = 5*x
-assert f.diff(x) == 5

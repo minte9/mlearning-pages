433372052bb4e6162d48586fe0a025569ad5358c
minte9 <catalinprescure@gmail.com>
2023-02-04 13:44:28 +0200

    mlearning-pages update

--- main/scikit/basis_expansion/basis_expansion.py
+++ main/scikit/basis_expansion/basis_expansion.py
@@ -1,6 +1,14 @@
 """ Basis Expansion (Polynomial Models)
-
 Adds non-linear features into to the linear model.
+
+Even though the fifth-degree polynomial model has the lowest
+SSR_training, it also has huge SSR_test.
+
+This is a good sign of overfitting.
+Another sign of overfitin is by evaluating the coeficients.
+
+Evaluate the weights (sum of coeficients)
+The higher the sum, the more the model tends to overfit.
 """
 
 import numpy as np
@@ -23,8 +31,6 @@ plt.title('Dataset (area, price)')
 plt.xlabel('Area (m^2)')
 plt.ylabel('Price (10,000$)')
 plt.legend(loc='best')
-plt.show()
-
 
 # ----------------------------------------------------
 # First-degree polynomial
@@ -54,26 +60,40 @@ plt.show()
 
 
 # ----------------------------------------------------
-# Second-degree polynomial
+# N-degree polynomial
 
 def pred_polinomial(d, x_unknown):
+
     degrees = d
     p = np.poly1d(np.polyfit(X, y, degrees))
     t = np.linspace(0, 100, 100)
-    print(p)        # 0.01443 x^2 - 0.4973 x + 31.9
-    print(p.coef)   # [1.44259995e-02 -4.97341625e-01  3.18982947e+01]
+    print(p)
+    print(p.coef)
 
+    # Plot train, test data and prediction line
     plt.figure(figsize=(6,4))
     plt.scatter(X, y, color='blue', label='Training set')
     plt.scatter(X2, y2, color='red', label='Test set')
     plt.plot(t, p(t), color='orange') # regression line
 
-    xa = x_unknown
-    ya = round(p(xa),2) # prediction
-    plt.scatter(xa, ya, color='r', marker='x')
-    plt.annotate(f'({xa}, {ya})', (xa+0.1, ya-10))
+    # Evaluate the model (sum of residuals)
+    SSR = sum((p(X) - y) ** 2)
+    SSR2 = sum((p(X2) - y2) ** 2)
+    SSR = round(SSR)
+    SSR2 = round(SSR2)
 
-    plt.title('Second-degree polynomial')
+    # Evaluate the weight (sum of coeficients)
+    # The hie
+    w = round(sum(abs(p.coef)))
+    print(w)
+
+    # plot prediction
+    xa = x_unknown
+    ya = round(p(xa),2)
+    plt.scatter(xa, ya, color='r', marker='x')
+    plt.annotate(f'({xa}, {ya}, SSR = {SSR}) SSR2 = {SSR2})', (xa+0.1, ya-10))
+
+    plt.title(f'{d}-degree polynomial')
     plt.legend(loc='best')
     plt.xlabel('Area (m^2)')
     plt.ylabel('Price (10,000$)')
@@ -84,4 +104,4 @@ def pred_polinomial(d, x_unknown):
 pred_polinomial(2, 50) # second-degree polynomial
 pred_polinomial(3, 50) # third-degree polynomial
 pred_polinomial(4, 50) # fourth-degree polynomial
-pred_polinomial(5, 50) # fifth-degree polynomial
+pred_polinomial(5, 50) # fift-degree polynomial

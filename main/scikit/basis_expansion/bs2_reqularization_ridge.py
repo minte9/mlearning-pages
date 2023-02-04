1bc0ef20c68fdc53beb7b3fdd8d483ab28052c75
minte9 <catalinprescure@gmail.com>
2023-02-04 16:41:09 +0200

    mlearning-pages update

--- main/scikit/basis_expansion/bs2_reqularization_ridge.py
+++ main/scikit/basis_expansion/bs2_reqularization_ridge.py
@@ -12,6 +12,9 @@ which penalizes large weight vectors.
 Reshape the train data to prevent numerical errors (to large or to small)
 By reshaping the data can be transform so that it has a mean of 0 
 and a standard deviation of 1
+
+When making predictions data must be transformed using the same 
+PolynomialFeatures transformation that was used to preprocess the training data.
 """
 
 from statistics import mode
@@ -49,14 +52,37 @@ print('Ridge coeficients: ', sum(model2.coef_))  # -7.221838297484756
 t_ = np.array(np.linspace(0, 100, 100)).reshape(-1, 1)
 t = PolynomialFeatures(degree=degree_).fit_transform(t_)
 
+
+# ------------------------------------------------------------------
+# Plotting
+
 # Plot train, test data and prediction line
 plt.figure(figsize=(6,4))
 plt.scatter(X, y, color='blue', label='Training set')
 plt.scatter(X2, y2, color='red', label='Test set')
+
+plt.title(f'{degree_}-degree polynomial / Ridge Regression')
 plt.plot(t_, model1.predict(t), '--', color='gray', label='Linear regression')
 plt.plot(t_, model2.predict(t), '-', color='orange', label='Ridge regularization')
-plt.title(f'{degree_}-degree polynomial / Ridge Regression')
+
+# Predictions
+x_unknown = 50
+xa = np.array([x_unknown]).reshape(-1,1)
+
+polyX = PolynomialFeatures(degree=degree_).fit_transform(xa)
+ya = model1.predict(polyX) # linear regression
+ya = round(ya[0], 2)
+
+polyX = PolynomialFeatures(degree=degree_).fit_transform(xa)
+yb = model2.predict(polyX) # ridge regression
+yb = round(yb[0], 2)
+
+plt.scatter(xa, ya, color='gray', marker='x')
+plt.scatter(xa, yb, color='red', marker='x')
+plt.annotate(f'({xa[0][0]}, {ya})', (xa+0.1, ya-5))
+plt.annotate(f'({xa[0][0]}, {yb})', (xa+0.1, yb+5))
+
 plt.xlim((0, 100))
 plt.ylim((0, 130))
-plt.legend(loc='best')
+plt.legend(loc='upper left')
 plt.show()

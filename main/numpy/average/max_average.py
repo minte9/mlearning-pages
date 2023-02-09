1faa04ac1a9dbf5c1e3f8e233612d56451f0325b
minte9 <catalinprescure@gmail.com>
2023-02-09 12:29:30 +0200

    mlearning-pages update

---
+++ main/numpy/average/max_average.py
@@ -0,0 +1,32 @@
+""" Min, Max, Mean
+We can apply operation along the axes (rows or columns).
+"""
+
+import numpy as np
+
+
+# --------------------------------------------
+# Max and Average
+
+matrix = np.array([
+    [1, 2, 3],
+    [4, 5, 6],
+    [7, 8, 9],
+])
+
+print(np.max(matrix))  # 9
+print(np.min(matrix))  # 1
+print(np.mean(matrix)) # 5.0
+
+assert np.max(matrix)  == 9     # passed
+assert np.min(matrix)  == 1     # passed
+assert np.mean(matrix) == 5     # passed
+
+print(np.max(matrix,  axis=1))    # [3 6 9]  # max in each row
+print(np.min(matrix,  axis=1))    # [1 4 7]  # min in each row 
+print(np.mean(matrix, axis=0))    # [4 5 6]  # average in each column
+print()
+
+np.max(matrix,   axis=1) == [3, 6, 9]   # passed
+np.min(matrix,   axis=1) == [1, 4, 7]   # passed
+np.mean(matrix,  axis=1) == [4, 5, 6]   # passed
--- main/numpy/vectorize/vectorize.py
+++ main/numpy/vectorize/vectorize.py
@@ -2,7 +2,6 @@
 Vectorization is essentialy a for loop and does not increase performance.
 By Broadcasting, Numpy allows operation on arrays, 
 even if their dimension are not the same.
-We can apply operation along the axes (rows or columns).
 """
 
 import numpy as np
@@ -44,30 +43,3 @@ print(matrix); print()
    # [[101 102 103]
    #  [104 105 106]
    #  [107 108 109]]
-
-
-# --------------------------------------------
-# Max and Average
-
-matrix = np.array([
-    [1, 2, 3],
-    [4, 5, 6],
-    [7, 8, 9],
-])
-
-print(np.max(matrix))  # 9
-print(np.min(matrix))  # 1
-print(np.mean(matrix)) # 5.0
-
-assert np.max(matrix)  == 9     # passed
-assert np.min(matrix)  == 1     # passed
-assert np.mean(matrix) == 5     # passed
-
-print(np.max(matrix,  axis=1))    # [3 6 9]  # max in each row
-print(np.min(matrix,  axis=1))    # [1 4 7]  # min in each row 
-print(np.mean(matrix, axis=0))    # [4 5 6]  # average in each column
-print()
-
-np.max(matrix,   axis=1) == [3, 6, 9]   # passed
-np.min(matrix,   axis=1) == [1, 4, 7]   # passed
-np.mean(matrix,  axis=1) == [4, 5, 6]   # passed

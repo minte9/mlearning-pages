61299aa2269498156811754ab7f2ccd2e4ab8d2a
minte9 <catalinprescure@gmail.com>
2023-02-10 12:42:24 +0200

    mlearning-pages update

---
+++ main/numpy/multiplication/addition.py
@@ -0,0 +1,24 @@
+""" Matrix addition
+For addiction (or substractiion) simply use + - operators
+"""
+
+import numpy as np
+
+A = np.array([
+    [1, 1],
+    [2, 2],
+])
+
+B = np.array([
+    [1, 1],
+    [3, 3],
+])
+
+C = np.add(A, A)    # first method
+C = A + B           # second method
+
+print(C)
+    # [2  2]
+    # [5  5]
+
+assert (np.add(A, A) == (A + A)) .all()   # passed
--- main/numpy/multiplication/multiplication.py
+++ main/numpy/multiplication/multiplication.py
@@ -4,55 +4,26 @@ For element-wise multiplication we use *
 
 import numpy as np
 
-
-# ----------------------------------------
-# Addition or substraction
-
-A = np.array([
-    [1, 1, 1],
-    [1, 1, 1],
-    [1, 1, 2],
-])
-
-B = np.array([
-    [1, 3, 1],
-    [1, 3, 1],
-    [1, 3, 8],
-])
-
-C = np.add(A, B)    # first method
-C = A + B           # second method
-
-print(C)
-    # [[ 2  4  2]
-    #  [ 2  4  2]
-    #  [ 2  4 10]]
-
-assert (np.add(A, B) == (A + B)) .all()   # passed
-print()
-
-
-# ---------------------------------------
-# Multiplication
-
 A = np.array([
     [1, 1],
-    [1, 2],
+    [2, 2],
 ])
 
 B = np.array([
-    [1, 3],
-    [1, 2]
+    [1, 1],
+    [3, 3],
 ])
 
 C = np.dot(A, B)    # first method
-D = A @ B           # second method
+C = A @ B           # second method
 E = A * B           # element-wise multiplication
 
-print(C) # [[2 5] [3 7]]
-print(D) # [[2 5] [3 7]]
-print(E) # [[1 3] [1 4]]
-print()
+print(C)
+    # [4 4]
+    # [8 8]
+print(E)  
+    # [1 1]
+    # [6 6]
 
-assert (np.dot(A, B) == (A @ B)) .all()     # passed
-assert (E[1, 1] == 4)                       # passed                     
+assert (np.dot(A, A) == (A @ A)) .all()     # passed
+assert (E[1, 1] == 6)                       # passed                     

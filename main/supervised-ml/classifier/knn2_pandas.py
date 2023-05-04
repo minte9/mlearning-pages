058d773bb9ca516332a0469a083dc239753ee3b8
minte9 <catalinprescure@gmail.com>
2023-05-04 09:54:54 +0300

    mlearning-pages update

--- main/supervised-ml/classifier/knn2_pandas.py
+++ main/supervised-ml/classifier/knn2_pandas.py
@@ -23,9 +23,8 @@ data = {
     'Apple', 'Apple', 'Mandarin', 'Lemon', 'Lemon', 'Lemon'
   ]
 } 
-
-df = pd.DataFrame(data) # transform dataset into a DataFrame
-print(df)
+# Transform dataset into a DataFrame
+df = pd.DataFrame(data) 
 
 X = df[['height', 'width']].values
 y = df.fruit.values
@@ -40,12 +39,37 @@ new_items = [[9, 3], [4, 5], [2, 5], [8, 9], [5, 7]]
 
 prediction  = knn.predict([new_item])
 predictions = knn.predict(new_items)
-print("Prediction label for item", new_item, ": \n", prediction)
-print("Precition labels for items", new_items, ":\n", predictions) 
+
+print(df, "\n")
+print("Prediction label for item", new_item, ": \n", prediction, "\n")
+print("Precition labels for items", new_items, ":\n", predictions, "\n") 
 
 """
-  Prediction label for item [[9, 3]] : 
-    ['Lemon']
-  Precition labels for items [[9, 3], [4, 5], [2, 5], [8, 9], [5, 7]] :
-    ['Lemon' 'Mandarin' 'Mandarin' 'Apple' 'Mandarin']
+		  height  width     fruit
+	0     3.91   5.76  Mandarin
+	1     7.09   7.69     Apple
+	2    10.48   7.32     Lemon
+	3     9.21   7.20     Lemon
+	4     7.95   5.90     Lemon
+	5     7.62   7.51     Apple
+	6     7.95   5.32  Mandarin
+	7     4.69   6.19  Mandarin
+	8     7.50   5.99     Lemon
+	9     7.11   7.02     Apple
+	10    4.15   5.60  Mandarin
+	11    7.29   8.38     Apple
+	12    8.49   6.52     Lemon
+	13    7.44   7.89     Apple
+	14    7.86   7.60     Apple
+	15    3.93   6.12     Apple
+	16    4.40   5.90  Mandarin
+	17    5.50   4.50     Lemon
+	18    8.10   6.15     Lemon
+	19    8.69   5.82     Lemon
+
+	Prediction label for item [9, 3] : 
+	 ['Lemon']
+
+	Precition labels for items [[9, 3], [4, 5], [2, 5], [8, 9], [5, 7]] :
+	 ['Lemon' 'Mandarin' 'Mandarin' 'Apple' 'Mandarin']
 """

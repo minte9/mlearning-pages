0b69475e9d65818c783550d1b69a72b18dbcf47a
minte9 <catalinprescure@gmail.com>
2023-05-04 09:59:50 +0300

    mlearning-pages update

--- main/supervised-ml/classifier/knn2_pandas.py
+++ main/supervised-ml/classifier/knn2_pandas.py
@@ -3,6 +3,8 @@
 Dataset contains heights, widths and labels (fruit name).
 The algorithm teach a function to map any combination 
 in order to predict an unknown fruit name.
+
+We use Pandas library to transform a json dataset into a DataFrame.
 """
 
 import pandas as pd

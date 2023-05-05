0d75fb1520096b7717383329e7852dac16c8ff7f
minte9 <catalinprescure@gmail.com>
2023-05-05 14:38:29 +0300

    mlearning-pages update

--- main/datasets/diabetes/diabetes_knn.py
+++ main/datasets/diabetes/diabetes_knn.py
@@ -1,7 +1,8 @@
-""" KNN Algorithm Application (Diabetes dataset)
+""" Diabetes dataset / Knn Algorithm
 
 Target is a quantitative measure of disease progression 
-one year after baseline. 
+one year after baseline. The features are: 
+age, sex, body mass index, average blood, etc.
 
 The target values are continuous not categorical.
 We need to divide the target values into two categories.
@@ -14,17 +15,8 @@ import numpy as np
 import matplotlib.pyplot as plt
 from sklearn.datasets import load_diabetes
 
-
-# ---------------------------------------------
-# Dataset
-
+# Diabetes dataset
 dataset = load_diabetes()
-
-print(dataset.keys())           # data, target, DESCR, feature_names
-print(dataset['DESCR'])         # age, sex, body mass index, average blood
-print(dataset['data'].shape)    # 442, 10
-print(dataset.target[0:7])      # 151.  75. 141. 206. 135.  97. 138.
-
 X = dataset.data
 y = dataset.target
 features = dataset.feature_names
@@ -38,18 +30,8 @@ y_binary = np.where(y > y.mean(), 1, 0)
 # Array containing class names (0 is 'OK', 1 is 'NOK')
 class_names = ['OK', 'NOK']
 
-
-# ------------------------------------------------
 # Knn algorithm
-
-# Define the unknown points
-xA = np.array([-0.075, 0.040])
-xB = np.array([0.10, 0.035])
-
-# Define the number of nearest neighbors (k=5)
-k = 5
-
-def knn(k, x_unknown):
+def knn(x_unknown, k=5):
     distances = np.sqrt(np.sum((X - x_unknown)**2, axis=1))
     keys = np.argsort(distances)
     knn_keys = keys[:k]
@@ -59,13 +41,16 @@ def knn(k, x_unknown):
     knn_class = class_names[knn_most_common_class]
     return knn_class, knn_keys
 
-knn_classA, knn_keysA = knn(k, xA)
-knn_classB, knn_keysB = knn(k, xB)
+# Unknown points
+xA = np.array([-0.075, 0.040])
+xB = np.array([0.10, 0.035])
+k_nearest = 5 # number of nearest neighbors (k=5)
 
+# Predictions
+knn_classA, knn_keysA = knn(xA, k_nearest)
+knn_classB, knn_keysB = knn(xB, k_nearest)
 
-# -----------------------------------------------
-# Plotting
-
+# Plot the two unknown points and dataset points
 fig, ax = plt.subplots()
 ax.set_xlabel('bmi (body mass index)')
 ax.set_ylabel('bp (average blood pressure)') 
@@ -74,12 +59,29 @@ plt.scatter(X[:, 0], X[:, 1], c=y_binary)
 plt.scatter(xA[0], xA[1], marker='o', color='g', label=knn_classA)
 plt.scatter(xB[0], xB[1], marker='x', color='r', label=knn_classB)
 
+# Plot lines to the nearest points
 for i in knn_keysA:
     plt.plot((xA[0], X[i][0]), (xA[1], X[i][1]), color='gray', linestyle='--')
-    
 for i in knn_keysB:
     plt.plot((xB[0], X[i][0]), (xB[1], X[i][1]), color='gray', linestyle='--')
 
-plt.title('Diabetes dataset - KNN')
+plt.title('Diabetes dataset - Knn')
 plt.legend()
 plt.show()
+
+print("Keys:", dataset.keys())
+print("Shape:", dataset['data'].shape)
+print("Target values examples:", dataset.target[0:7])
+print(dataset['DESCR'])
+
+"""
+    Keys: dict_keys(['data', 'target', 'frame', 'DESCR', ...])
+    Shape: (442, 10)
+    Target values examples: [151.  75. 141. 206. 135.  97. 138.]
+    Diabetes dataset
+    ----------------
+    Ten baseline variables, age, sex, body mass index, average blood
+    pressure, and six blood serum measurements were obtained for each of n =
+    442 diabetes patients, as well as the response of interest, a
+    quantitative measure of disease progression one year after baseline.
+"""

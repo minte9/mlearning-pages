9ebd44ce707bc4c205363f335dbfb82071d27afd
minte9 <catalinprescure@gmail.com>
2023-05-05 20:31:15 +0300

    mlearning-pages update

--- main/numpy/matrices/vector.py
+++ main/numpy/matrices/vector.py
@@ -3,13 +3,13 @@ One-dimensional arrays
 """
 
 import numpy as np
+
 row = np.array([1, 2, 3])
 column = np.array([
     [1],
     [4],
     [3],
 ])
-
 print("Row vector: \n", row)
 print("Column vector: \n", column)
 

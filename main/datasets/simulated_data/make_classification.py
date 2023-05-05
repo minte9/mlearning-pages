676e8ee431822353fe589c2aaa8301ab82be6b98
minte9 <catalinprescure@gmail.com>
2023-05-05 15:38:36 +0300

    mlearning-pages update

--- main/datasets/simulated_data/make_classification.py
+++ main/datasets/simulated_data/make_classification.py
@@ -15,14 +15,17 @@ features, target = make_classification(
     random_state = 1
 )
 
-print(features[0:3])
-    # [[ 1.30022717 -0.7856539 ]
-    #  [ 1.44184425 -0.56008554]
-    #  [-0.84792445 -1.36621324]]
-
-print(target[:10])
-    # [0 1 1 1 2 2 2 1 0 0]
-
 plt.scatter(features[:, 0], features[:, 1], c=target)
 plt.title('Make classification - Simultated dataset')
 plt.show()
+
+print("Features[0:3]:\n", features[0:3])
+print("Target:[:10]:", target[:10])
+
+"""
+	Features[0:3]:
+	 [[ 1.30022717 -0.7856539 ]
+	 [ 1.44184425 -0.56008554]
+	 [-0.84792445 -1.36621324]]
+	Target:[:10]: [1 1 0 0 0 1 1 1 0 1]
+"""

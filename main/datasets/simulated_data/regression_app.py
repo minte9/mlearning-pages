b025be29293e90e9551b1640f7e29b067ffe15b2
minte9 <catalinprescure@gmail.com>
2023-05-05 16:32:25 +0300

    mlearning-pages update

--- main/datasets/simulated_data/regression_app.py
+++ main/datasets/simulated_data/regression_app.py
@@ -16,23 +16,21 @@ import pandas as pd
 # Sample dataset
 X, y = make_regression(
     n_samples = 100, 
-    n_features = 1, 
+    n_features = 1, # Employ years of experience
     n_informative = 1, 
-    n_targets = 1,
+    n_targets = 1,  # Employ's salary
     noise = 10, 
     coef = False, 
     random_state = 0
 )
 
 # Scale feature and target
-X = np.interp(X, (X.min(), X.max()), (0, 20))           # employ years of experience
-y = np.interp(y, (y.min(), y.max()), (10000, 200000))   # employ's salary
-
-# Data frame
-df = pd.DataFrame(data={'Experience': X.flatten(), 'Salary': y})
+X = np.interp(X, (X.min(), X.max()), (0, 20))           
+y = np.interp(y, (y.min(), y.max()), (10000, 200000))   
 
 # Fit a linear regression model
 reg = LinearRegression().fit(X, y)
+
 
 # Plot dataset points
 plt.scatter(X, y, label='training data')
@@ -47,11 +45,22 @@ plt.text(10, 25000, r'y = %0.2f + %0.2f x' % (reg.intercept_, reg.coef_[0]))
 plt.xlabel('Years of experience')
 plt.ylabel('Salary')
 plt.legend()
-# plt.show()
+plt.show()
 
-print("Training employ_experiency:\n", X[:2]) # [[-0.35955316]]
-print("Trraining employ_salary:\n", y[:2]) # [-19.95588561]
-print(df.head(2))
-    #  Experience         Salary
-    #    9.096218   95224.004179
-    #   14.637429  132619.663538
+
+# Print data frame
+df = pd.DataFrame(data={'Experience': X.flatten(), 'Salary': y})
+print(df.head(10))
+"""
+	   Experience         Salary
+	0    9.096218   95224.004179
+	1   14.637429  132619.663538
+	2   12.255808  123760.689176
+	3    7.215160   98496.528556
+	4    6.905628   80966.199869
+	5   12.427999  138646.723320
+	6    6.534503   62290.952298
+	7   12.363590  129242.508929
+	8   11.451010  132720.442525
+	9    9.295277   93053.397973
+"""

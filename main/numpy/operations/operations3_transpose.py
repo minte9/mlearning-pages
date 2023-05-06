d236c5148cc8dacc7ef98f89ff84f5a98776bc41
minte9 <catalinprescure@gmail.com>
2023-05-06 10:05:44 +0300

    mlearning-pages update

--- main/numpy/operations/operations3_transpose.py
+++ main/numpy/operations/operations3_transpose.py
@@ -18,11 +18,16 @@ assert M[0, 1] == M_transposed[1, 0]
 assert M[1, 0] == M_transposed[0, 1]
 assert M[1, 1] == M_transposed[1, 1]
 
+print("Matrix =\n", M)
 print("Matrix transposed =\n", M_transposed)
 
 """
+    Matrix =
+     [[1 2 3]
+      [4 5 6]
+      [7 8 9]]
     Matrix transposed =
-    [[1 4 7]
-     [2 5 8]
-     [3 6 9]]
+     [[1 4 7]
+      [2 5 8]
+      [3 6 9]]
 """

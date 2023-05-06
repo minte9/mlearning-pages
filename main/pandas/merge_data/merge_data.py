20c6e39b6101bc339b27f312ebf0645cfe8c002f
minte9 <catalinprescure@gmail.com>
2023-05-06 16:30:53 +0300

    mlearning-pages update

--- main/pandas/merge_data/merge_data.py
+++ main/pandas/merge_data/merge_data.py
@@ -15,36 +15,34 @@ sales = pd.DataFrame()
 sales['id_employee'] = [3, 4, 5, 6]
 sales['total_sales'] = [10000, 20000, 30000, 40000]
 
+D1 = pd.merge(employes, sales, on='id_employee')
+D2 = pd.merge(employes, sales, on='id_employee', how='outer')
+D3 = pd.merge(employes, sales, left_on='id_employee', right_on='id_employee')
 
-# Inner join (default)
-T = pd.merge(employes, sales, on='id_employee')
-print(T.to_markdown())
+print("Inner join (default):"); print(D1.to_markdown(), "\n")
+print("Outer join (how):"); print(D2.to_markdown(), "\n")
+print("Column name (in each) to merge on:"); print(D3.to_markdown(), "\n")
 
-# |    |   id_employee | name    |   total_sales |
-# |---:|--------------:|:--------|--------------:|
-# |  0 |             3 | Bob     |         10000 |
-# |  1 |             4 | Michael |         20000 |
+"""
+Inner join (default):
+|    |   id_employee | name    |   total_sales |
+|---:|--------------:|:--------|--------------:|
+|  0 |             3 | Bob     |         10000 |
+|  1 |             4 | Michael |         20000 | 
 
+Outer join (how):
+|    |   id_employee | name    |   total_sales |
+|---:|--------------:|:--------|--------------:|
+|  0 |             1 | John    |           nan |
+|  1 |             2 | Mary    |           nan |
+|  2 |             3 | Bob     |         10000 |
+|  3 |             4 | Michael |         20000 |
+|  4 |             5 | nan     |         30000 |
+|  5 |             6 | nan     |         40000 | 
 
-# Outer join (how)
-T = pd.merge(employes, sales, on='id_employee', how='outer')
-print(T.to_markdown())
-
-# |    |   id_employee | name    |   total_sales |
-# |---:|--------------:|:--------|--------------:|
-# |  0 |             1 | John    |           nan |
-# |  1 |             2 | Mary    |           nan |
-# |  2 |             3 | Bob     |         10000 |
-# |  3 |             4 | Michael |         20000 |
-# |  4 |             5 | nan     |         30000 |
-# |  5 |             6 | nan     |         40000 |
-
-
-# Column name (in each) to merge on
-T = pd.merge(employes, sales, left_on='id_employee', right_on='id_employee')
-print(T.to_markdown())
-
-# |    |   id_employee | name    |   total_sales |
-# |---:|--------------:|:--------|--------------:|
-# |  0 |             3 | Bob     |         10000 |
-# |  1 |             4 | Michael |         20000 |
+Column name (in each) to merge on:
+|    |   id_employee | name    |   total_sales |
+|---:|--------------:|:--------|--------------:|
+|  0 |             3 | Bob     |         10000 |
+|  1 |             4 | Michael |         20000 | 
+"""

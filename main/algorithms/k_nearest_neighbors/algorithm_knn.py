6ae497dcb212f10b89849d97cdb1a0846524dd15
minte9 <catalinprescure@gmail.com>
2023-05-06 18:42:08 +0300

    mlearning-pages update

--- main/algorithms/k_nearest_neighbors/algorithm_knn.py
+++ main/algorithms/k_nearest_neighbors/algorithm_knn.py
@@ -1,6 +1,6 @@
 """ K-Nearest Neighbors Algorithm
 
-In ML, the computer uses data to learn the best f(x) 
+In ML, the computer uses data to learn the best f(x) fit
 In classical algorithms, the f(x) is provided by the programmer
 
 1. Load training dataset
@@ -14,56 +14,78 @@ In classical algorithms, the f(x) is provided by the programmer
 import numpy as np
 import matplotlib.pyplot as plt
 
-X = np.array([
+# Train dataset
+X = [
     [2, 2], [2, 2.5], [2.5, 2.5], [2.5, 2], [2.25, 2.25],
     [3, 3], [3, 3.5], [3.5, 3.5], [3.5, 3], [3.25, 3.25],
     [4, 4], [4, 4.5], [4.5, 4.5], [4.5, 4], [4.25, 4.25],
-])
-y = np.array([
+]
+y = [
     1, 1, 1, 1, 1,
     2, 2, 2, 2, 2,
     3, 3, 3, 3, 3,
-])
+]
 
-k = 3 # number of k-nearest neighbors to use
-xA = np.array([3.6, 1.8]) # new point with unknown class
+class KNeighborsClassifier:
 
-distances = np.sqrt(np.sum((X - xA)**2, axis=1)) # axis=1 means rows of X
-keys = np.argsort(distances)
-knn_keys = keys[:k]
-knn_classes = y[knn_keys]
-knn_most_common_class = np.bincount(knn_classes) # the number of occurrences 
-knn_class = knn_most_common_class.argmax() # max value in array
+    def __init__(self, n_neighbors):
+        self.k = n_neighbors
 
-print("Distances: \n",distances)
-print("Keys ordered by distances: \n", keys)
-print("Nearest neighbors keys: \n", knn_keys)
-print("Nearest neighbors classes: \n", knn_classes)
-print("Algorithm class response: \n", knn_class)
+    def fit(self, X_train, y_train):
+        self.X = np.array(X_train)
+        self.y = np.array(y_train)
+    
+    def predict(self, x_unknown):
+        X = self.X
+        y = self.y
+        k = self.k
+        z = np.array(x_unknown)
 
-"""
-Distances:                  [1.61245155 1.74642492 1.30384048 ...
-Keys ordered by distances:  [3  8  2  5  4  ...
-Nearest neighbors keys:     [3 8 2]
-Nearest neighbors classes:  [1 2 1]
-Algorithm class response:   1
-"""
+        # Algorithm
+        # --------------------------------------------------------------
 
-# -----------------------------------------------
-# Plotting
+        # Square distances matrix
+        SD = np.sqrt(np.sum((X - z)**2, axis=1)) # axis=1 means rows of X
+        keys = np.argsort(SD)
 
-fig, ax = plt.subplots()
-ax.set_xlabel('x1')
-ax.set_ylabel('x2')
+        # Neighbors target matrix
+        keys_knn = keys[:k]
+        targets_knn = y[keys_knn]
 
-plt.scatter(X[:, 0], X[:, 1], c=y)
-plt.scatter(xA[0], xA[1], marker='x', color='r', label='Class =%s' %knn_class)
+        # Optim target
+        most_common = np.bincount(targets_knn) # by number of occurrences 
+        result = most_common.argmax() # max value in array
 
-for i in knn_keys:
-    plt.plot((xA[0], X[i][0]), (xA[1], X[i][1]), color='gray', linestyle='--')
+        # --------------------------------------------------------------
 
-plt.title('K-nearest Neigbors')
-plt.xlim(0, 6)
-plt.ylim(0, 6)
-plt.legend()
-plt.show()
+        print("Square distances: \n", SD, "\n")
+        print("Keys ordered by distances: \n", keys, "\n")
+        print("Nearest neighbors keys: \n", keys_knn, "\n")
+        print("Nearest neighbors targets: \n", targets_knn, "\n")
+        print("Algorithm target response: \n", result, "\n")
+
+        # Plot the point and lines to th k neighbors
+        fig, ax = plt.subplots()
+        ax.set_xlabel('x1')
+        ax.set_ylabel('x2')
+
+        plt.scatter(X[:, 0], X[:, 1], c=y)
+        plt.scatter(z[0], z[1], marker='x', color='r', label='Class =%s' %result)
+
+        for i in keys_knn:
+            plt.plot((z[0], X[i][0]), (z[1], X[i][1]), color='gray', linestyle='--')
+
+        plt.title('K-nearest Neigbors')
+        plt.xlim(0, 6)
+        plt.ylim(0, 6)
+        plt.legend()
+        plt.show()
+
+        return result
+
+knn = KNeighborsClassifier(n_neighbors=3)
+knn.fit(X, y)
+
+x_unknown = [3.6, 1.8]  
+knn_class = knn.predict(x_unknown)   
+print("Class prediction for", x_unknown, "=", knn_class)

a40b6719a154fa3cefadd9d76d3a545c3f525fd2
minte9 <catalinprescure@gmail.com>
2023-05-06 16:18:31 +0300

    mlearning-pages update

--- main/pandas/group_rows/group_rows.py
+++ main/pandas/group_rows/group_rows.py
@@ -11,42 +11,41 @@ import pandas as pd
 import pathlib
 
 DIR = pathlib.Path(__file__).resolve().parent / '../_data/'
-df = pd.read_csv(DIR / 'titanic.csv')
+FILE = DIR / 'titanic.csv'
+df = pd.read_csv(FILE)
 
+t1 = df.groupby('Sex').mean(numeric_only=True).to_markdown()
+t2 = df.groupby('Sex').count().to_markdown()
+t3 = df.groupby('Sex')['Survived'].count().to_markdown()
+t4 = df.groupby(['Sex', 'Survived'])['Age'].mean().to_markdown()
 
-print("Grouped by Sex averages:")
-print(df.groupby('Sex').mean(numeric_only=True).to_markdown())
+print("Grouped by Sex averages:"); print(t1, "\n")
+print("Grouped by Sex:"); print(t2, "\n")
+print("Grouped by Sex (count only Survived):"); print(t3, "\n")
+print("Group by two columns (average):"); print(t3, "\n")
 
-# | Sex    |     Age |   Survived |   SexCode |
-# |:-------|--------:|-----------:|----------:|
-# | female | 29.3964 |   0.666667 |         1 |
-# | male   | 31.0143 |   0.166863 |         0 |
+"""
+Grouped by Sex averages:
+| Sex    |     Age |   Survived |   SexCode |
+|:-------|--------:|-----------:|----------:|
+| female | 29.3964 |   0.666667 |         1 |
+| male   | 31.0143 |   0.166863 |         0 | 
 
+Grouped by Sex:
+| Sex    |   Name |   PClass |   Age |   Survived |   SexCode |
+|:-------|-------:|---------:|------:|-----------:|----------:|
+| female |    462 |      462 |   288 |        462 |       462 |
+| male   |    851 |      851 |   468 |        851 |       851 | 
 
-print("Grouped by Sex:")
-print(df.groupby('Sex').count().to_markdown())
+Grouped by Sex (count only Survived):
+| Sex    |   Survived |
+|:-------|-----------:|
+| female |        462 |
+| male   |        851 | 
 
-# | Sex    |   Name |   PClass |   Age |   Survived |   SexCode |
-# |:-------|-------:|---------:|------:|-----------:|----------:|
-# | female |    462 |      462 |   288 |        462 |       462 |
-# | male   |    851 |      851 |   468 |        851 |       851 |
-
-
-print("Grouped by Sex (count only Survived):")
-print(df.groupby('Sex')['Survived'].count().to_markdown())
-
-# | Sex    |   Survived |
-# |:-------|-----------:|
-# | female |        462 |
-# | male   |        851 |
-
-
-print("Group by two columns (average):")
-print(df.groupby(['Sex', 'Survived'])['Age'].mean().to_markdown())
-
-# |               |     Age |
-# |:--------------|--------:|
-# | ('female', 0) | 24.9014 |
-# | ('female', 1) | 30.8671 |
-# | ('male', 0)   | 32.3208 |
-# | ('male', 1)   | 25.9519 |
+Group by two columns (average):
+| Sex    |   Survived |
+|:-------|-----------:|
+| female |        462 |
+| male   |        851 | 
+"""

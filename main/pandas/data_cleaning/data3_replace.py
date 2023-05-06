00ee0086dceb3165c4b8c00fcdb4c466b4c8e86a
minte9 <catalinprescure@gmail.com>
2023-05-06 13:43:59 +0300

    mlearning-pages update

--- main/pandas/data_cleaning/data3_replace.py
+++ main/pandas/data_cleaning/data3_replace.py
@@ -10,49 +10,41 @@ import pathlib
 DIR = pathlib.Path(__file__).resolve().parent / '../_data/'
 df = pd.read_csv(DIR / 'titanic.csv')
 
+R1 = df['Sex'].replace("female", "Woman")
+R2 = df['Sex'].replace(['female', 'male'], ['Woman', 'Man'])
+R3 = df.replace(1, 'one')
+R4 = df.replace(r'1st', 'First', regex=True)
 
-# Replace on value
-R = df['Sex'].replace("female", "Woman")
-print("Replace in one column:")
-print(R.head(2).to_markdown())
+print("Replace in one column:"); print(R1.head(2).to_markdown(), "\n")
+print("Replace multiple values:"); print(R2.head(5).to_markdown(), "\n")
+print("Replace all:"); print(R3.head(2).to_markdown(), "\n")
+print("Regex replace:"); print(R4.head(2).to_markdown(), "\n")
 
-# |    | Sex   |
-# |---:|:------|
-# |  0 | Woman |
-# |  1 | Woman |
+"""
+Replace in one column:
+|    | Sex   |
+|---:|:------|
+|  0 | Woman |
+|  1 | Woman | 
 
+Replace multiple values:
+|    | Sex   |
+|---:|:------|
+|  0 | Woman |
+|  1 | Woman |
+|  2 | Man   |
+|  3 | Woman |
+|  4 | Man   | 
 
-# Replace multipe values
-R = df['Sex'].replace(['female', 'male'], ['Woman', 'Man'])
-print("Replace multiple values:")
-print(R.head(5).to_markdown())
+Replace all:
+|    | Name                         | PClass   |   Age | Sex    | Survived   | SexCode   |
+|---:|:-----------------------------|:---------|------:|:-------|:-----------|:----------|
+|  0 | Allen, Miss Elisabeth Walton | 1st      |    29 | female | one        | one       |
+|  1 | Allison, Miss Helen Loraine  | 1st      |     2 | female | 0          | one       | 
 
-# |    | Sex   |
-# |---:|:------|
-# |  0 | Woman |
-# |  1 | Woman |
-# |  2 | Man   |
-# |  3 | Woman |
-# |  4 | Man   |
-
-
-# Replace all
-R = df.replace(1, 'one')
-print("Replace all:")
-print(R.head(2).to_markdown())
-
-# |    | Name           | PClass   |   Age | Sex    | Survived   | SexCode   |
-# |---:|:---------------|:---------|------:|:-------|:-----------|:----------|
-# |  0 | Allen, Miss Eli| 1st      |    29 | female | one        | one       |
-# |  1 | Allison, Miss H| 1st      |     2 | female | 0          | one       |
-
-
-# Regex
-R = df.replace(r'1st', 'First', regex=True)
-print("Regex replace:") 
-print(R.head(2).to_markdown())
-
-# |    | Name           | PClass   |   Age | Sex    |   Survived |   SexCode |
-# |---:|:---------------|:---------|------:|:-------|-----------:|----------:|
-# |  0 | Allen, Miss Eli| First    |    29 | female |          1 |         1 |
-# |  1 | Allison, Miss H| First    |     2 | female |          0 |         1 |
+Regex replace:
+|    | Name                         | PClass   |   Age | Sex    |   Survived |   SexCode |
+|---:|:-----------------------------|:---------|------:|:-------|-----------:|----------:|
+|  0 | Allen, Miss Elisabeth Walton | First    |    29 | female |          1 |         1 |
+|  1 | Allison, Miss Helen Loraine  | First    |     2 | female |          0 |         1 | 
+"""

d6e3852d8031b09087cace86f8ae7dab63de78b8
minte9 <catalinprescure@gmail.com>
2023-05-06 12:09:55 +0300

    mlearning-pages update

--- main/pandas/read_data/read_csv.py
+++ main/pandas/read_data/read_csv.py
@@ -5,23 +5,27 @@ The source can be URL or FILE
 import pandas as pd
 import pathlib
 
-# Read from URL
 URL = 'https://raw.githubusercontent.com/chrisalbon/sim_data/master/data.csv'
-df1 = pd.read_csv(URL)
-
-# Read from FILE
 DIR = pathlib.Path(__file__).resolve().parent 
-df2 = pd.read_csv(DIR / '../_data/01.csv')
+FILE = DIR / '../_data/01.csv'
 
+df1 = pd.read_csv(URL)
+df2 = pd.read_csv(FILE)
+
+print("Read csv from url:")
 print(df1.head(2).to_markdown(), "\n")
+
+print("Read csv from file:")
 print(df2.head(2).to_markdown())
 
 """
+    Read csv from url:
     |    |   integer | datetime            |   category |
     |---:|----------:|:--------------------|-----------:|
     |  0 |         5 | 2015-01-01 00:00:00 |          0 |
-    |  1 |         5 | 2015-01-01 00:00:01 |          0 | 
+    |  1 |         5 | 2015-01-01 00:00:01 |          0 |
 
+    Read csv from file:
     |    |   integer | datetime            |   category |
     |---:|----------:|:--------------------|-----------:|
     |  0 |         5 | 2015-01-01 00:00:00 |          0 |

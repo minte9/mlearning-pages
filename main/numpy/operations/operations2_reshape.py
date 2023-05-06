546e2da3d1504cf66a0c584ec757f870b8f10d86
minte9 <catalinprescure@gmail.com>
2023-05-06 09:57:03 +0300

    mlearning-pages update

--- main/numpy/operations/operations2_reshape.py
+++ main/numpy/operations/operations2_reshape.py
@@ -9,21 +9,40 @@ Flatten transform a matrix into a one-dimensional array.
 
 import numpy as np
 
-matrix = np.array([
+M = np.array([
     [1, 2, 3],
     [4, 5, 6],
     [7, 8, 9],
     [10, 11, 12],
 ])
 
-A = matrix.reshape(2, 6)
-B = matrix.reshape(1, -1)
-C = matrix.flatten()
+A = M.reshape(2, 6)
+B = M.reshape(1, -1)
+C = M.flatten()
 
-print(A) # [[ 1  2  3  4  5  6] [ 7  8  9 10 11 12]]
-print(B) # [[ 1  2  3  4  5  6  7  8  9 10 11 12]]
-print(C) # [  1  2  3  4  5  6  7  8  9 10 11 12]
+assert M.size == A.size
+assert M.size == B.size
+assert M.size == C.size
 
-assert matrix.size == A.size  # passed
-assert matrix.size == B.size  # passed
-assert matrix.size == C.size  # passed
+print("Matrix =\n", M)
+print("Matrix reshape (2,6) =\n", A)
+print("Matrix reshape (1,-1) =\n", B)
+print("Matrix flatten =\n", C)
+
+"""
+	Matrix =
+	 [[ 1  2  3]
+	  [ 4  5  6]
+	  [ 7  8  9]
+	 [10 11 12]]
+
+	Matrix reshape (2,6) =
+	 [[ 1  2  3  4  5  6]
+	  [ 7  8  9 10 11 12]]
+
+	Matrix reshape (1,-1) =
+	 [[ 1  2  3  4  5  6  7  8  9 10 11 12]]
+     
+	Matrix flatten =
+	 [ 1  2  3  4  5  6  7  8  9 10 11 12]
+"""

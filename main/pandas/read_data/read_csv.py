78cd231829415b82bcfe7f0725929cc47d2aa481
minte9 <catalinprescure@gmail.com>
2023-05-06 12:06:11 +0300

    mlearning-pages update

--- main/pandas/read_data/read_csv.py
+++ main/pandas/read_data/read_csv.py
@@ -7,15 +7,21 @@ import pathlib
 
 # Read from URL
 URL = 'https://raw.githubusercontent.com/chrisalbon/sim_data/master/data.csv'
-dataframe = pd.read_csv(URL)
+df1 = pd.read_csv(URL)
 
 # Read from FILE
 DIR = pathlib.Path(__file__).resolve().parent 
-dataframe = pd.read_csv(DIR / '../_data/01.csv')
+df2 = pd.read_csv(DIR / '../_data/01.csv')
 
-print(dataframe.head(2).to_markdown())
+print(df1.head(2).to_markdown(), "\n")
+print(df2.head(2).to_markdown())
 
 """
+    |    |   integer | datetime            |   category |
+    |---:|----------:|:--------------------|-----------:|
+    |  0 |         5 | 2015-01-01 00:00:00 |          0 |
+    |  1 |         5 | 2015-01-01 00:00:01 |          0 | 
+
     |    |   integer | datetime            |   category |
     |---:|----------:|:--------------------|-----------:|
     |  0 |         5 | 2015-01-01 00:00:00 |          0 |

7459d85aafcb6817b9c1d3be9c1ad976dddc5267
minte9 <catalinprescure@gmail.com>
2023-05-07 18:51:17 +0300

    mlearning-pages update

--- main/calculus/derivatives/04_slope_plot.py
+++ main/calculus/derivatives/04_slope_plot.py
@@ -3,7 +3,6 @@ import numpy as np
 
 # Falling object s(t) = 16t^2
 a = 16
-
 X = np.arange(-5, 5, 0.1)
 Y = a*(X**2)
 
@@ -16,6 +15,7 @@ for x in range(2, 6):
     y = a*(x**2)
     m = 2*a*x
     b = y - m*x
+    print(f"Instant speed for x = {x} is {y} with slope {m} and intercept {b}")
 
     # Plot points
     plt.plot(x, y, 'rx')
@@ -25,8 +25,6 @@ for x in range(2, 6):
     t = str(x)
     plt.plot(X, m*X + b, label='s(' + t + ') = ' + str(m))
 
-    # Print results
-    print(f"Instant speed for x = {x} is {y} with slope {m} and intercept {b}")
 
 # Plot figure
 plt.title('s(t) = 16t^2')

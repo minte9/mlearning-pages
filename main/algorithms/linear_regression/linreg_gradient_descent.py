594fceaaaa23ed546c0777328416e46e382cee1b
minte9 <catalinprescure@gmail.com>
2023-05-07 13:19:03 +0300

    mlearning-pages update

--- main/algorithms/linear_regression/linreg_gradient_descent.py
+++ main/algorithms/linear_regression/linreg_gradient_descent.py
@@ -3,14 +3,16 @@
 The algorithm is used to find the best-fit line that models 
 the relationship between x and y.
 
-Gradient descent optimization updates the slope and intercept 
-of the line on each iteration of the algorithm.
-
 Gradient descent is an iterative optimization algorithm that starts 
 with an initial guess for the slope and the intercept and updates them 
 in the direction of steepest descent of the cost function until convergence.
 
 The slope or gradient of a function in (x,y) point is the derivative.
+The cost function measures the error between the predicted and actual values.
+
+By iteratively updating the m and b values in the direction of the negative gradient, 
+the algorithm finds the values that minimize the mean squared error, 
+which corresponds to the best fit line for the training data.
 """
 
 import numpy as np
@@ -32,13 +34,16 @@ num_iterations = 1000
 
 # Gradient descent optimization
 for i in range(num_iterations):
-
+    
+    # Cost function
     y_pred = m*x + b
-    error = y - y_pred
+    error = y - y_pred 
 
+    # Partial derivatives of the cost function with respect to m and b
     m_derivative = -(2/len(x)) * sum(x * error)
     b_derivative = -(2/len(x)) * sum(error)
 
+    # Update the m and b values in the direction of the negative gradient
     m = m - learning_rate * m_derivative
     b = b - learning_rate * b_derivative
 

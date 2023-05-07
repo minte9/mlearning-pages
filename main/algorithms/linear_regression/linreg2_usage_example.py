9e606ee952c07efb10a997649e2e35b3de3ef674
minte9 <catalinprescure@gmail.com>
2023-05-07 13:37:21 +0300

    mlearning-pages update

--- main/algorithms/linear_regression/linreg2_usage_example.py
+++ main/algorithms/linear_regression/linreg2_usage_example.py
@@ -21,7 +21,7 @@ class LinearRegression:
     def fit(self, X_train, Y_train, learning_rate=0.01, num_iterations=1000):
         lr = LinearRegression()
 
-        # -----------------------------------------------
+        # -----------------------------------
 
         x = X_train
         y = Y_train
@@ -37,7 +37,7 @@ class LinearRegression:
             m -= learning_rate * m_derivative
             b -= learning_rate * b_derivative
 
-        # -----------------------------------------------
+        # -----------------------------------
 
         lr.coef_.append(m)
         lr.intercept_ = b

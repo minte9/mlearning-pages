39e40e36e60bf88b478017e5aac4402b980e277f
minte9 <catalinprescure@gmail.com>
2023-05-07 18:01:12 +0300

    mlearning-pages update

--- main/calculus/derivatives/03_simple_polynomials.py
+++ main/calculus/derivatives/03_simple_polynomials.py
@@ -1,22 +1,28 @@
-""" Differentiation of Simple Polynomials
-Contributions from each term
+""" Devivatives / Polynomials
+
+Differentiation of Simple Polynomials adds contributions
+from each term:
     y   = ax^2 + bx + c
     y'  = 2ax + b
     y'' = 2a
 """
 
 from sympy import *
-x = Symbol('x')
 
+x = Symbol('x')
 a = 2
 b = 3
 c = 4
 
 y = a*x**2 + b*x + c
 d = y.diff(x)
-print(y)            # function
-print(y.diff(x))    # first derivative
-print(d.diff(x))    # second derivative
-    # 2*x**2 + 3*x + 4
-    # 4*x + 3
-    # 4
+
+print("Function:", y)
+print("First derivative:", y.diff(x))
+print("Second derivative:", d.diff(x))
+
+"""
+    Function:           2*x**2 + 3*x + 4
+    First derivative:   4*x + 3
+    Second derivative:  4
+"""

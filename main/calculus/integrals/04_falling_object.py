2c870a1476cfce763701dcae3da4dff757127fcf
minte9 <catalinprescure@gmail.com>
2023-05-08 10:48:58 +0300

    mlearning-pages update

--- main/calculus/integrals/04_falling_object.py
+++ main/calculus/integrals/04_falling_object.py
@@ -95,7 +95,9 @@ def update(frame):
 
 fig, ax = plt.subplots()
 ani = FuncAnimation(fig, update, frames=np.arange(10, 51, 1), repeat=True)
-plt.show() # ani.save('1427_falling_ball.gif', writer='imagemagick', fps=10)
+plt.show() 
+
+# ani.save('1427_falling_ball.gif', writer='imagemagick', fps=10)
 
 """
     Acceleration = 32

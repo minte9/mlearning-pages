714bb53f39dcf3d1044debcec052cd4500e53f22
minte9 <catalinprescure@gmail.com>
2023-05-08 10:18:51 +0300

    mlearning-pages update

--- main/calculus/integrals/02_definite_integral.py
+++ main/calculus/integrals/02_definite_integral.py
@@ -8,6 +8,8 @@ import numpy as np
 import scipy.integrate as spi
 import matplotlib.pyplot as plt
 
+# ---------------------------------------------------------------
+
 # Function to be integrated
 def f(x):
     return x**2
@@ -20,8 +22,7 @@ def integral_approximation_B(f, a, b): #scipy
     approx, error = spi.quad(f, a, b)
     return approx
 
-
-# Integral bounds
+# Integral boudaries (lower and upper limits)
 a = 0
 b = 1
 
@@ -30,11 +31,13 @@ range = np.arange(a, b + .0001, .0001)
 Y = f(range)
 
 # Integral approximation
-approx_A = integral_approximation_A(Y, a, b) # numpy
-approx_B = integral_approximation_B(f, a, b) # scipy
+approx_A = integral_approximation_A(Y, a, b)
+approx_B = integral_approximation_B(f, a, b)
+
+# ---------------------------------------------------------------
+
 print(approx_A) # 0.33335
 print(approx_B) # 0.33333333333333337
-
 
 # Plotting the function
 x = np.linspace(0, 1, 100)
@@ -43,7 +46,7 @@ y = f(x)
 fig, ax = plt.subplots()
 ax.plot(x, y, label="f(x) = x^2")
 ax.fill_between(x, y, 0, where=(x >= 0) & (x <= 1), color="gray", alpha=0.5, 
-label=f'I = %s' %round(approx_B, 4))
+label=f'I (Area) = %s' %round(approx_B, 4))
 ax.set_xlabel("x")
 ax.set_ylabel("y")
 ax.legend()

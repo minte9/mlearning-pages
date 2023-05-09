8301db13cb7219e4aacef90770a2dd7481836aa7
minte9 <catalinprescure@gmail.com>
2023-05-09 20:39:07 +0300

    mlearning-pages update

--- main/supervised-ml/classifier/knn.py
+++ main/supervised-ml/classifier/knn.py
@@ -7,12 +7,15 @@ We are able now to predict the label of a new data point.
 
 from sklearn.neighbors import KNeighborsClassifier
 
+# Training dataset
 X = [[0,0], [1,1], [2,2], [3,3]]    
 y = [0, 1, 0, 1]
 
+# Train the model
 knn = KNeighborsClassifier(n_neighbors=3)
 knn.fit(X, y)
 
+# Make predictions
 x_unknown = [1,2]        
 prediction = knn.predict([x_unknown])   
 print("Label prediction for", x_unknown, "=", prediction)

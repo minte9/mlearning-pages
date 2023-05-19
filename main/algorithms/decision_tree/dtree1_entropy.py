4ac4bc2ff1f04fa17b8357f453da6ae5a484197c
minte9 <catalinprescure@gmail.com>
2023-05-19 12:41:55 +0300

    mlearning-pages update

--- main/algorithms/decision_tree/dtree1_entropy.py
+++ main/algorithms/decision_tree/dtree1_entropy.py
@@ -17,9 +17,9 @@ fruits = pd.Series(lst)
 probs = fruits.value_counts(normalize=True)
 probs2 = [3/7, 2/7, 2/7] # by hand
 
-# Entropy (Shannon model) and Information gain
+# Entropy (Shannon model)
 entropy = -1 * np.sum(probs * np.log2(probs))
-gini_index = 1 - np.sum(np.square(probs))
+gini_index = 1 - np.sum(np.square(probs)) # information gain
 
 outputs = [
     ["Fruits narray:", fruits.values],
@@ -29,21 +29,17 @@ outputs = [
     ["Information gain:", gini_index],
 ]
 for v in outputs: 
-    print("\n", v[0], "\n ", v[1])
+    print(v[0], "\n ", v[1])
 
 """
     Fruits narray: 
      ['apple' 'apple' 'apple' 'orange' 'orange' 'banana' 'banana']
-
     Probability distribution - value_count(): 
      [0.42857143 0.28571429 0.28571429]
-
     Probability distribution - by hand: 
      [0.42857142857142855, 0.2857142857142857, 0.2857142857142857]
-
     Entropy: 
      1.5566567074628228
-
     Information gain: 
      0.653061224489796
 """

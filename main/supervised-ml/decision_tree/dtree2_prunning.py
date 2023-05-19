8e8a84f03670d3b12ecc0207bfa15e9c483f2b4a
minte9 <catalinprescure@gmail.com>
2023-05-19 19:10:35 +0300

    mlearning-pages update

--- main/supervised-ml/decision_tree/dtree2_prunning.py
+++ main/supervised-ml/decision_tree/dtree2_prunning.py
@@ -1,4 +1,4 @@
-""" Decision Trees / Feature importance
+""" Decision Trees / Prunning
 
 Insteed of looking at the whole tree, we can select only the most useful properties.
 To summarize the feature importance we use `tree.feature_importance`.
@@ -23,20 +23,20 @@ X1, X2, y1, y2 = train_test_split(
     dataset.data, dataset.target, stratify=dataset.target, random_state=42)
 
 # Pre-prunning
-tree = DecisionTreeClassifier(max_depth=4, random_state=0)
-tree.fit(X1, y1)
+dtree = DecisionTreeClassifier(max_depth=4, random_state=0)
+dtree.fit(X1, y1)
 
 # Output
 n = dataset.data.shape[1]
 plt.subplots_adjust(left=0.28)
-plt.barh(np.arange(n), tree.feature_importances_, align='center')
+plt.barh(np.arange(n), dtree.feature_importances_, align='center')
 plt.yticks(np.arange(n), dataset.feature_names)
 plt.xlabel("Feature importance")
 plt.ylabel("Feature")
 plt.ylim(-1, n)
 plt.show()
 
-print('Feature importances: \n', tree.feature_importances_)
+print('Feature importances: \n', dtree.feature_importances_)
 
 """
     Feature importances: 

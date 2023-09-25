449a96ae4e83c7ca295c193ce7018cc6c758dd46
minte9 <catalinprescure@gmail.com>
2023-09-25 19:33:49 +0300

    mlearning-pages update

---
+++ main/algorithms/gradient_descent/gd1_regression_lines.py
@@ -0,0 +1,34 @@
+""" Linear Regression lines with know intercept parameter (b = -18)
+"""
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+# Training Dataset
+X = np.array([30, 46, 60, 65, 77, 95]).reshape(6,1)
+Y = np.array([31, 30, 80, 49, 70, 118])
+
+# Define a range of slope values (parameter 'a') to explore
+A = np.linspace(-2, 4.5, 13) # 13 values
+
+# Output results
+print("Slope range: \n", A)
+
+# Create a plot for the training data and various linear regression lines
+fig, ax = plt.subplots()
+plt.ylim(0, 140)
+plt.xlim(0, 140)
+
+# Plot training data points
+ax.plot(X, Y, 'o', color='g', label='training data') 
+
+for i in range(len(A)):
+    msg ='f(x) = -18 + %sx' % A[i].round(1)
+
+    # Plot linear regression lines
+    ax.plot(X, -18 + A[i]*X, label = msg) 
+
+plt.xlabel("x")
+plt.ylabel("f(x)")  
+plt.legend()
+plt.show()

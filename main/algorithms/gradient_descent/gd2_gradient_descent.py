be408e37fd35784b6a936a575c382f5295ea99ab
minte9 <catalinprescure@gmail.com>
2023-09-25 20:05:08 +0300

    mlearning-pages update

--- main/algorithms/gradient_descent/gd2_gradient_descent.py
+++ main/algorithms/gradient_descent/gd2_gradient_descent.py
@@ -1,7 +1,5 @@
-""" Gradient descent
-Algorithm starts with a random value of the parameter a, b=-18
-Then, it finds the direction in which the function
-descrease faster and takes a step in that direction, then repeat.
+""" Gradient descent algorithm
+Find the optimal value of a linear regression parameter 'a' for a given dataset
 """
 
 import matplotlib.pyplot as plt
@@ -11,20 +9,26 @@ import numpy as np
 X = np.array([30, 46, 60, 65, 77, 95]).reshape(6,1)
 Y = np.array([31, 30, 80, 49, 70, 118])
 
-# ------------------------------------------------------------
-
 # Cost function
 def J(a):
     J = 0
-    for i in range(len(X)): # number of train points
-        J += (Y[i] - (a*X[i] + -18))**2
+
+    # Loop through each data point
+    for i in range(len(X)):
+
+        # Calculate the squared error
+        J += (Y[i] - (a*X[i] + -18))**2 
+
     return J
 
 # Derivative of the cost function
 def dJ(a):
     dJ = 0
     for i in range(len(X)):
+
+        # Calculate the derivative
         dJ += -2*X[i]*(Y[i] - (a*X[i] + -18)) # d(x^2) = 2x
+
     return dJ.item()
 
 # Gradient descent
@@ -32,17 +36,15 @@ def gradient_descent(X, Y, b=-18, lr=0.00001, loops=15):
     a = 0
     for i in range(15):
 
-        # J(a) derivative is used to find where the SSR is the lowest
+        # Update 'a' using the gradient of the cost function
         d = dJ(a)
         a = a - d*lr
         
-        # print(f'Step {i+1} a = {round(a, 5)}')
+        print(f'Step {i+1} a = {round(a, 5)}')
     return round(a, 5)
 
 # Result
 optim_a = gradient_descent(X, Y)
-
-# ------------------------------------------------------------
 
 # Compute values to print and plot
 a = 0       # start value
@@ -58,7 +60,7 @@ fig, ax = plt.subplots()
 A = np.linspace(-2, 4.5, 23) # 21 values
 ax.plot(A, J(A), label='J(a) = sum(R(X)^2)') # J(a)
 
-# Mimin SSR(a), or optim a
+# Mark the minimum SSR(a) (optim_a)
 ax.plot(optim_a, J(optim_a), 'o', color='g', label='optim_a = 1.3029')
 
 # Draw points (as gradient descends)
@@ -72,16 +74,16 @@ ax.plot([a0,  a1], [J(0), J(a1)], color='r')
 ax.plot([a1, a2], [J(a1), J(a2)], color='r')
 ax.plot([a2, a3], [J(a2), J(a3)], color='r')
 
-# Show figure
+# Customize the plot
 plt.xlim(-2, 5)
 plt.ylim(-10000, 70000)
 plt.xlabel("a")
 plt.ylabel("SSR(a)")  
-
 ax.axhline(y=0, color='k')
 ax.axvline(x=0, color='k')
 plt.legend()
 
+# Show the plot
 plt.show()
 
 # Print results
@@ -92,11 +94,19 @@ print('Step 3 a =', round(a3, 5), "\n")
 print("Gradient descent optim_a slope: \n", round(optim_a, 4))
 
 """
-    Derivative of cost function J(0) =  -67218
     Step 1 a = 0.67218
     Step 2 a = 0.99758
-    Step 3 a = 1.15511 
-
-    Gradient descent optim_a slope: 
-        1.3029
+    Step 3 a = 1.15511
+    Step 4 a = 1.23137
+    Step 5 a = 1.26829
+    Step 6 a = 1.28616
+    Step 7 a = 1.29481
+    Step 8 a = 1.299
+    Step 9 a = 1.30102
+    Step 10 a = 1.30201
+    Step 11 a = 1.30248
+    Step 12 a = 1.30271
+    Step 13 a = 1.30282
+    Step 14 a = 1.30288
+    Step 15 a = 1.3029
 """

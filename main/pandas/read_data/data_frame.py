6619be5c773ec3ab57c5929c63324bc52318e22b
minte9 <catalinprescure@gmail.com>
2023-10-06 09:49:19 +0300

    mlearning-pages update

---
+++ main/pandas/read_data/data_frame.py
@@ -0,0 +1,24 @@
+""" Pandas / DataFrame
+"""
+
+import pandas as pd
+
+data = {
+    'apples': [3, 2, 0, 1],
+    'oranges': [0, 3, 7, 2],
+    'available': ['yes', 'no', 'yes', 'no'],
+}
+df = pd.DataFrame(data)
+
+print("DataFrame:")
+print(df.to_markdown())
+
+"""
+    DataFrame:
+    |    |   apples |   oranges | available   |
+    |---:|---------:|----------:|:------------|
+    |  0 |        3 |         0 | yes         |
+    |  1 |        2 |         3 | no          |
+    |  2 |        0 |         7 | yes         |
+    |  3 |        1 |         2 | no          |
+"""

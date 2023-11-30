2c01ca8653c37ec6e724d440fbfdfd8480e258ba
minte9 <catalinprescure@gmail.com>
2023-11-30 12:58:59 +0200

    mlearning-pages update

--- main/algorithms/knn_classifier/knn_algorithm.py
+++ main/algorithms/knn_classifier/knn_algorithm.py
@@ -1,20 +1,3 @@
-""" K-Nearest Neighbors Classifier / Algorithm
-
-The algorithm works by finding the k nearest neighbors to the input data point, 
-and then assigning the output label based on the majority 
-vote of the k-nearest neighbors.
-
-In classical algorithms, the f(x) is provided by the programmer.
-In ML, the computer uses data to learn the best f(x) fit.
-
-1. Load training dataset
-2. Select a value for k
-3. Calculate distances to the new point
-4. Select the k-nearest points
-5. Get the most common class
-6. Assign the new point to that class
-"""
-
 import numpy as np
 import matplotlib.pyplot as plt
 
@@ -29,23 +12,24 @@ y = np.array([
     2, 2, 2, 2, 2,
     3, 3, 3, 3, 3,
 ])
-
-k_nearest = 3
 x_unknown = np.array([3.6, 1.8])
+
+# Choose the number of neighbors to be used with the algorithm
+k = 3 
 
 # --------------------------------------------------------------
 
-# Square distances matrix
-SD = np.sqrt(np.sum((X - x_unknown)**2, axis=1)) # axis=1 means rows of X
-keys = np.argsort(SD)
+# Square distances (matrix)
+SD = np.sqrt(np.sum((X - x_unknown)**2, axis=1))    # Calculate Euclidean distances
+keys = np.argsort(SD)                               # Sort distances to find closest points
 
-# Neighbors target matrix
-keys_knn = keys[:k_nearest]
-targets_knn = y[keys_knn]
+# Neighbors target (matrix)
+keys_knn = keys[:k]                 # Select k-nearest indices
+targets_knn = y[keys_knn]           # Get labels of k-nearest neighbors
 
 # Optim target
-most_common = np.bincount(targets_knn) # by number of occurrences 
-knn_class = most_common.argmax() # max value in array
+most_common = np.bincount(targets_knn)      # Count occurrences of each label
+knn_class = most_common.argmax()            # Predict most common label
 
 # --------------------------------------------------------------
 
@@ -54,10 +38,12 @@ fig, ax = plt.subplots()
 ax.set_xlabel('x1')
 ax.set_ylabel('x2')
 
+# Plot training data points with respective labels
 z = x_unknown
 plt.scatter(X[:, 0], X[:, 1], c=y)
 plt.scatter(z[0], z[1], marker='x', color='r', label='Class =%s' %knn_class)
 
+# Plot dashed lines between unknown point and its k-nearest neighbors
 for i in keys_knn:
     plt.plot((z[0], X[i][0]), (z[1], X[i][1]), color='gray', linestyle='--')
 
@@ -67,6 +53,7 @@ plt.ylim(0, 6)
 plt.legend()
 plt.show()
 
+# Output informations
 print("Square distances:", SD)
 print("Keys ordered by distances:", keys)
 print("Nearest neighbors keys:", keys_knn)

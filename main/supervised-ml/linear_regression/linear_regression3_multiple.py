8b06ee891502cb98cf2b129bdc53e1fec89f2d65
minte9 <catalinprescure@gmail.com>
2023-12-02 12:54:25 +0200

    mlearning-pages update

--- main/supervised-ml/linear_regression/linear_regression3_multiple.py
+++ main/supervised-ml/linear_regression/linear_regression3_multiple.py
@@ -1,15 +1,11 @@
-""" Linear Regression / multiple parameters
-h(x) = ax + by + cz + ... 
-"""
-
-from os import X_OK
-import numpy as np, sys
-import matplotlib.pyplot as plt
 from sklearn.linear_model import LinearRegression
+import matplotlib.pyplot as plt
+import numpy as np, sys
 import pandas as pd
-import pathlib
 
+import pathlib
 DIR = pathlib.Path(__file__).resolve().parent
+
 with open(DIR / 'data/real_estate.csv') as file:
     df = pd.read_csv(file)
 
@@ -34,7 +30,7 @@ X1 = [2013.17, 13, 732.85, 0, 24.98, 121.53]     # price: 39 (train data)
 X2 = [2013.58, 16.6, 323.69, 6, 24.98, 121.54]   # price: 51 (train data)
 X3 = [2013.17, 33, 732.85, 0, 24.98, 121.53]     # ?
 
-print(df, '\n')
+print(df.head())
 print('Predict training item1, price =', r.predict([X1]).round(1).item())
 print('Predict training item2, price =', r.predict([X2]).round(1).item())
 print('Predict unknow item, price =',    r.predict([X3]).round(1).item())
@@ -46,14 +42,8 @@ print('Predict unknow item, price =',    r.predict([X3]).round(1).item())
 	2      3             2013.583          13.3  ...
 	3      4             2013.500          13.3  ...
 	4      5             2012.833           5.0  ...
-	..   ...                  ...           ...  ...
-	409  410             2013.000          13.7  ...
-	410  411             2012.667           5.6  ...
-	411  412             2013.250          18.8  ...
-	412  413             2013.000           8.1  ...
-	413  414             2013.500           6.5  ...
 
-	[414 rows x 8 columns] 
+	[5 rows x 8 columns]
 
 	Predict training item1, price = 38.8
 	Predict training item2, price = 48.5

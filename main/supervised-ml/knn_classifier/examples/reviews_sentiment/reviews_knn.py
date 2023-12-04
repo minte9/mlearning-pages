e9666b6e83148db337fda7c3f678ac684d015dd3
minte9 <catalinprescure@gmail.com>
2023-12-04 14:19:59 +0200

    mlearning-pages update

--- main/supervised-ml/knn_classifier/examples/reviews_sentiment/reviews_knn.py
+++ main/supervised-ml/knn_classifier/examples/reviews_sentiment/reviews_knn.py
@@ -21,6 +21,7 @@ from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.model_selection import train_test_split
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.metrics import classification_report
+from icecream import ic
 
 # Dataset
 DIR = pathlib.Path(__file__).resolve().parent
@@ -53,16 +54,32 @@ def steming_words(A):
     A = [[porter.stem(w) for w in words] for words in A]
     return A
 
-def debug(s1, s2=None, debug=False):
+def debug(s1, s2=None, debug=True):
     if debug:
         print(s1, s2)
 
-debug("Initial Dataset:\n", df)
-df['Review'] = clear_text(df['Review']);            debug("Cleanning:\n", df)
-df['Review'] = remove_punctuation(df['Review']);    debug("Remove Punctuation:\n", df)
-df['Words']  = tokenize_words(df['Review']);        debug("Tokenize Words:\n", df['Words'])
-df['Words']  = remove_stopwords(df['Words']);       debug("Remove Stop Words:\n", df['Words'])
-df['Words']  = steming_words(df['Words']);          debug("Stem Words:\n", df['Words'])
+ic("Initial dataset")
+ic(df.head())
+
+ic("Clear Text (remove numbers, lower, strip)")
+df['Review'] = clear_text(df['Review'])
+ic(df.head())
+
+ic("Remove Punctuation")
+df['Review'] = remove_punctuation(df['Review'])
+ic(df.head())
+
+ic("Tokenize Words")
+df['Words']  = tokenize_words(df['Review'])
+ic(df['Words'].head())
+
+ic("Remove Stop Words")
+df['Words']  = remove_stopwords(df['Words'])
+ic(df['Words'].head())
+
+ic("Stem Words")
+df['Words']  = steming_words(df['Words'])
+id(df['Words'].head())
 
 def build_dataset(df):
     # E = CountVectorizer() # Bag of words
@@ -76,9 +93,9 @@ def build_dataset(df):
     dataset = pd.concat([T, df['Sentiment']], axis=1)
     return dataset
 
-# Final dataset
+id("Final dataset")
 dataset = build_dataset(df)
-debug("Final Dataset:\n\n", dataset)
+ic(dataset.head())
 
 # Features and label
 X = dataset.iloc[:, :-1] # all the columns except the last
@@ -104,16 +121,60 @@ print("Score on Test:", knn.score(X2, y2).round(2))
 print("Report:", classification_report(y2, y_pred, zero_division=0))
 
 """
-    Test data:       negative negative positive positive negative positive positive
-    Prediction:      positive positive positive positive negative positive positive
+    ic| 'Initial dataset'
+    ic| df.head():                                               Review Sentiment
+                0  When you go to the 'Look inside' option, you c...  negative
+                1  I wouldn't recommend if completely new to codi...  negative
+                2  I bought this book for a master's data science...  positive
+                3  Please review the table of contents before pur...  positive
+                4  If you are following a parallel course of ML t...  positive
+    ic| 'Clear Text (remove numbers, lower, strip)'
+    ic| df.head():                                               Review Sentiment
+                0  when you go to the 'look inside' option, you c...  negative
+                1  i wouldn't recommend if completely new to codi...  negative
+                2  i bought this book for a master's data science...  positive
+                3  please review the table of contents before pur...  positive
+                4  if you are following a parallel course of ml t...  positive
+    ic| 'Remove Punctuation'
+    ic| df.head():                                               Review Sentiment
+                0  when you go to the look inside option you can ...  negative
+                1  i wouldnt recommend if completely new to codin...  negative
+                2  i bought this book for a masters data science ...  positive
+                3  please review the table of contents before pur...  positive
+                4  if you are following a parallel course of ml t...  positive
+    ic| 'Tokenize Words'
+    ic| df['Words'].head(): 0    [when, you, go, to, the, look, inside, option,...
+                            1    [i, wouldnt, recommend, if, completely, new, t...
+                            2    [i, bought, this, book, for, a, masters, data,...
+                            3    [please, review, the, table, of, contents, bef...
+                            4    [if, you, are, following, a, parallel, course,...
+                            Name: Words, dtype: object
+    ic| 'Remove Stop Words'
+    ic| df['Words'].head(): 0    [go, look, inside, option, see, code, colors, ...
+                            1    [wouldnt, recommend, completely, new, coding, ...
+                            2    [bought, book, masters, data, science, class, ...
+                            3    [please, review, table, contents, purchasing, ...
+                            4    [following, parallel, course, ml, book, great,...
+                            Name: Words, dtype: object
+    ic| 'Stem Words'
+    ic| dataset.head():    abl  abund  access  accompani   account   ...  yet  youll  your  Sentiment
+                        0  0.0    0.0     0.0        0.0  0.000000   ...  0.0    0.0   0.0   negative
+                        1  0.0    0.0     0.0        0.0  0.220705   ...  0.0    0.0   0.0   negative
+                        2  0.0    0.0     0.0        0.0  0.000000   ...  0.0    0.0   0.0   positive
+                        3  0.0    0.0     0.0        0.0  0.000000   ...  0.0    0.0   0.0   positive
+                        4  0.0    0.0     0.0        0.0  0.000000   ...  0.0    0.0   0.0   positive
+                        
+                        [5 rows x 566 columns]
+    Test data:       negative positive positive negative positive negative negative
+    Prediction:      positive positive positive negative positive positive positive
     Score on Train: 0.79
-    Score on Test: 0.71
+    Score on Test: 0.57
     Report:               precision    recall  f1-score   support
 
-        negative       1.00      0.33      0.50         3
-        positive       0.67      1.00      0.80         4
+        negative       1.00      0.25      0.40         4
+        positive       0.50      1.00      0.67         3
 
-        accuracy                           0.71         7
-    macro avg       0.83      0.67      0.65         7
-    weighted avg       0.81      0.71      0.67         7
+        accuracy                           0.57         7
+       macro avg       0.75      0.62      0.53         7
+    weighted avg       0.79      0.57      0.51         7
 """

2c52672b918812636a103853dd561c0779c37101
minte9 <catalinprescure@gmail.com>
2023-12-14 10:05:21 +0200

    mlearning-pages update

---
+++ main/supervised-ml/count_vectorizer/combine_features.py
@@ -0,0 +1,55 @@
+""" Knn / Movie recommendation system (scikit)
+"""
+
+import pathlib
+import pandas as pd
+import numpy as np
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.metrics.pairwise import cosine_similarity
+
+DIR = pathlib.Path(__file__).resolve().parent
+movies = pd.read_csv(DIR / 'data/movies_dataset2.csv')
+
+# New combined feature
+def combine_features(row):
+    return str(row['genres']) + " " + str(row['cast']) + " " + str(row['keywords'])
+
+movies['combined_features'] = movies.apply(combine_features, axis=1)
+
+# Similarity
+cv = CountVectorizer()
+count_matrix = cv.fit_transform(movies['combined_features'])
+cosine_similarity = cosine_similarity(count_matrix)
+
+# Find the 10 most similiar movies
+def predict_movies(name):
+    index = movies[movies['original_title'].str.contains(name)].index[0]
+    similar_movies = list(enumerate(cosine_similarity[index]))
+
+    # Sort by score in descending order
+    similar_sorted = sorted(similar_movies, key=lambda x: x[1], reverse=True)[1:]
+
+    # Output results
+    for neighbor in similar_sorted[:10]:
+        movie_index = neighbor[0]
+        data = movies.iloc[movie_index]
+
+        original_title = data['original_title']
+        genres = data['genres']
+        rating = str(data['vote_average'])
+        print(original_title + " | " + genres + " | Rating: " + rating) 
+        
+predict_movies('Avatar')
+
+"""
+    Guardians of the Galaxy | Action Science Fiction Adventure | Rating: 7.9
+    Star Trek Into Darkness | Action Adventure Science Fiction | Rating: 7.4
+    Star Trek Beyond | Action Adventure Science Fiction | Rating: 6.6
+    Alien | Horror Action Thriller Science Fiction | Rating: 7.9
+    Star Wars: Clone Wars (Volume 1) | Action ... Fiction | Rating: 8.0
+    Planet of the Apes | Thriller Science Fiction Action Adventure | Rating: 5.6
+    Moonraker | Action Adventure Thriller Science Fiction | Rating: 5.9
+    Galaxy Quest | Comedy Family Science Fiction | Rating: 6.9
+    Gravity | Science Fiction Thriller Drama | Rating: 7.3
+    Jupiter Ascending | Science Fiction Fantasy Action Adventure | Rating: 5.2
+"""
--- main/supervised-ml/count_vectorizer/count_vectorizer.py
+++ main/supervised-ml/count_vectorizer/count_vectorizer.py
@@ -1,55 +1,37 @@
-""" Knn / Movie recommendation system (scikit)
+""" Count Vectorizer
+Represent texts as vectors and compute similarity.
+The word `london` occurs 2 times in A and 1 time in B.
+We can find the cos similarity between these two vectors.
 """
 
-import pathlib
-import pandas as pd
-import numpy as np
 from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.metrics.pairwise import cosine_similarity
 
-DIR = pathlib.Path(__file__).resolve().parent
-movies = pd.read_csv(DIR / 'data/movies_dataset2.csv')
+A = 'London Paris London'
+B = 'Paris Paris London'
 
-# New combined feature
-def combine_features(row):
-    return str(row['genres']) + " " + str(row['cast']) + " " + str(row['keywords'])
-
-movies['combined_features'] = movies.apply(combine_features, axis=1)
-
-# Similarity
 cv = CountVectorizer()
-count_matrix = cv.fit_transform(movies['combined_features'])
-cosine_similarity = cosine_similarity(count_matrix)
+matrix = cv.fit_transform([A, B])
+similarity_scores = cosine_similarity(matrix)
 
-# Find the 10 most similiar movies
-def predict_movies(name):
-    index = movies[movies['original_title'].str.contains(name)].index[0]
-    similar_movies = list(enumerate(cosine_similarity[index]))
-
-    # Sort by score in descending order
-    similar_sorted = sorted(similar_movies, key=lambda x: x[1], reverse=True)[1:]
-
-    # Output results
-    for neighbor in similar_sorted[:10]:
-        movie_index = neighbor[0]
-        data = movies.iloc[movie_index]
-
-        original_title = data['original_title']
-        genres = data['genres']
-        rating = str(data['vote_average'])
-        print(original_title + " | " + genres + " | Rating: " + rating) 
-        
-predict_movies('Avatar')
+print("Features names: \n", cv.get_feature_names_out())
+print("Matrix vectorized: \n", matrix)
+print("Matrix array: \n", matrix.toarray(), "\n")
+print ("Similarity scores: \n", similarity_scores)
 
 """
-    Guardians of the Galaxy | Action Science Fiction Adventure | Rating: 7.9
-    Star Trek Into Darkness | Action Adventure Science Fiction | Rating: 7.4
-    Star Trek Beyond | Action Adventure Science Fiction | Rating: 6.6
-    Alien | Horror Action Thriller Science Fiction | Rating: 7.9
-    Star Wars: Clone Wars (Volume 1) | Action ... Fiction | Rating: 8.0
-    Planet of the Apes | Thriller Science Fiction Action Adventure | Rating: 5.6
-    Moonraker | Action Adventure Thriller Science Fiction | Rating: 5.9
-    Galaxy Quest | Comedy Family Science Fiction | Rating: 6.9
-    Gravity | Science Fiction Thriller Drama | Rating: 7.3
-    Jupiter Ascending | Science Fiction Fantasy Action Adventure | Rating: 5.2
+	Features names: 
+	 ['london' 'paris']
+	Matrix vectorized: 
+	   (0, 0)       2
+	  (0, 1)        1
+	  (1, 0)        1
+	  (1, 1)        2
+	Matrix array: 
+	 [[2 1]
+	 [1 2]] 
+
+	Similarity scores: 
+	 [[1.  0.8]
+	 [0.8 1. ]]
 """

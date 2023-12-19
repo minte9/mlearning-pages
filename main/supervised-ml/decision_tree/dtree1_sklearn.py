853c3f804ece3377a45a905dfa7005b31efb87c6
minte9 <catalinprescure@gmail.com>
2023-12-19 15:06:49 +0200

    mlearning-pages update

--- main/supervised-ml/decision_tree/dtree1_sklearn.py
+++ main/supervised-ml/decision_tree/dtree1_sklearn.py
@@ -12,21 +12,21 @@ DIR = pathlib.Path(__file__).resolve().parent
 df = pd.read_csv(DIR / 'data/play_tennis.csv')
 
 """
-outlook,temp,humidity,windy,play
-sunny,hot,high,false,no
-sunny,hot,high,true,no
-overcast,hot,high,false,yes
-rainy,mild,high,false,yes
-rainy,cool,normal,false,yes
-rainy,cool,normal,true,no
-overcast,cool,normal,true,yes
-sunny,mild,high,false,no
-sunny,cool,normal,false,yes
-rainy,mild,normal,false,yes
-sunny,mild,normal,true,yes
-overcast,mild,high,true,yes
-overcast,hot,normal,false,yes
-rainy,mild,high,true,no
+    outlook,temp,humidity,windy,play
+    sunny,hot,high,false,no
+    sunny,hot,high,true,no
+    overcast,hot,high,false,yes
+    rainy,mild,high,false,yes
+    rainy,cool,normal,false,yes
+    rainy,cool,normal,true,no
+    overcast,cool,normal,true,yes
+    sunny,mild,high,false,no
+    sunny,cool,normal,false,yes
+    rainy,mild,normal,false,yes
+    sunny,mild,normal,true,yes
+    overcast,mild,high,true,yes
+    overcast,hot,normal,false,yes
+    rainy,mild,high,true,no
 """
 
 # Encode lables

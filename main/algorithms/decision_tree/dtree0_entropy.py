8e4dc513985dbf5c2edfad99dc8e542d535f40e6
minte9 <catalinprescure@gmail.com>
2023-12-22 15:58:24 +0200

    mlearning-pages update

--- main/algorithms/decision_tree/dtree0_entropy.py
+++ main/algorithms/decision_tree/dtree0_entropy.py
@@ -1,59 +1,40 @@
-""" Decision Tree / Entropy
-"""
-
 import pandas as pd
 import numpy as np
+from icecream import ic
 
 # Set the initial traning data
 A = ['apple']*1 + ['orange']*2 + ['banana']*2
 B = ['apple']*5 + ['orange']*2 + ['banana']*0
+ic(A, B)
 
-print("Datasets:")
-print("A =", A)
-print("B =", B)
-
-# Probability (by hand)
-P1 = [1/5, 2/5, 2/5] 
-P2 = [5/7, 2/7, 0/7]
-
-print("\n Probability (by hand):")
-print("P1 =", P1)
-print("P2 =", P2)
-
-# Probability (with pandas)
-A = pd.Series(A)
-B = pd.Series(B)
-
-P1 = A.value_counts(normalize=True)
-P2 = B.value_counts(normalize=True)
-
-print("\n Probability (with pandas):")
-print("P1 =", P1.values)
-print("P2 =", P2.values)
+# Probability
+P1 = pd.Series(A).value_counts(normalize=True)
+P2 = pd.Series(B).value_counts(normalize=True)
+ic(P1, P2)
 
 # Entropy (Shannon model)
+P1 = P1.values
+P2 = P2.values
 H1 = -1 * np.sum(P1 * np.log2(P1))
 H2 = -1 * np.sum(P2 * np.log2(P2))
+ic(H1, H2);
+
 assert H1 > H2
 
-print("\n Entropy:")
-print("H1 =", H1)
-print("H2 =", H2)
+ic("A entropy > B entropy | There is more disorder in A than B")
+ic("Assertion passed");
 
 """
-    Datasets:
-    A = ['apple' 'orange' 'orange' 'banana' 'banana']
-    B = ['apple' 'apple' 'apple' 'apple' 'apple' 'orange' 'orange']
-
-     Probability distributions (by hand):
-    [0.2, 0.4, 0.4]
-    [0.7142857142857143, 0.2857142857142857, 0.0]
-
-    Probability distributions (with pandas):
-    [0.4 0.4 0.2]
-    [0.71428571 0.28571429]
-
-    Entropies:
-    1.5219280948873621
-    0.863120568566631
+    ic| A: ['apple', 'orange', 'orange', 'banana', 'banana']
+        B: ['apple', 'apple', 'apple', 'apple', 'apple', 'orange', 'orange']
+    ic| P1: orange    0.4
+            banana    0.4
+            apple     0.2
+            dtype: float64
+        P2: apple     0.714286
+            orange    0.285714
+            dtype: float64
+    ic| H1: 1.5219280948873621, H2: 0.863120568566631
+    ic| 'A entropy > B entropy | There is more disorder in A than B'
+    ic| 'Assertion passed'
 """
